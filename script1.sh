27c1489fda653cc9790199e4415cd39a7e29ce5e
Anuradha <anuradhahadkar30@gmail.com>
2024-08-25 23:55:43 +0530

    New changes commited to branch

--- script1.sh
+++ script1.sh
@@ -1,6 +1,7 @@
+echo "Good morning"
 echo "We are trying this practical from linux EC2"
 echo "We are from batch-25 and learning git/gihub"
 echo "We are learning Git/Github"
-echo "Thank you Alll"
+echo "Thank you All"
 
 
